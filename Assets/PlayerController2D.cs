49405a1e626750de8023a3ab0ff60814d1f2769b
AriyanK <60440392+AriyanK@users.noreply.github.com>
2022-07-03 16:22:07 -0500

    Basic dash

--- Assets/PlayerController2D.cs
+++ Assets/PlayerController2D.cs
@@ -8,24 +8,34 @@ public class PlayerController2D : MonoBehaviour
     Rigidbody2D rb2d;
     SpriteRenderer spriteRenderer;
 
-    bool isGrounded;
+    bool isGrounded;                    //BASIC STATES
     bool isShooting;
     bool isRunning;
     bool isFacingLeft;
 
-    [SerializeField]
+    [SerializeField]                    //GROUND CHECKS
     Transform groundCheck;
     [SerializeField]
     Transform groundCheckL;
     [SerializeField]
     Transform groundCheckR;
 
-    [SerializeField]
+    [SerializeField]                    //MOVEMENT VARIABLES
     private float runSpeed = 1.5f;
     [SerializeField]
     private float jumpSpeed = 5f;
     [SerializeField]
     private float shotDelay = .5f;
+
+    [SerializeField]                   //DASH VARIABLES
+    private float dashSpeed;
+    [SerializeField]
+    private float dashTime;
+    private float startDashTime = 10f;
+
+    [SerializeField]
+    private float dashDistance = 10f;
+    bool isDashing;
 
     private float jumpTimeCounter;
     [SerializeField]
@@ -35,7 +45,7 @@ public class PlayerController2D : MonoBehaviour
     private bool doubleJump = false;
     
 
-    [SerializeField]
+    [SerializeField]                    //BULLET VARIABLES
     Transform bulletSpawnPos;
     [SerializeField]
     GameObject bullet;
@@ -50,7 +60,7 @@ public class PlayerController2D : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground")) ||
+        if(Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground")) ||                
           (Physics2D.Linecast(transform.position, groundCheckL.position, 1 << LayerMask.NameToLayer("Ground"))) ||
           (Physics2D.Linecast(transform.position, groundCheckR.position, 1 << LayerMask.NameToLayer("Ground"))))
         {
@@ -65,7 +75,7 @@ public class PlayerController2D : MonoBehaviour
             }
         }
 
-        if(Input.GetKey("d") || Input.GetKey("right")){
+        if(Input.GetKey("d") || Input.GetKey("right")){                 //MOVING LEFT AND RIGHT
             rb2d.velocity = new Vector2(runSpeed, rb2d.velocity.y);
             isRunning = true;
             isFacingLeft = false;
@@ -102,26 +112,46 @@ public class PlayerController2D : MonoBehaviour
             rb2d.velocity = new Vector2(0, rb2d.velocity.y);
         }
 
-        
+        /* if(Input.GetKey("k")){
+            if(dashTime <= 0){
+                Debug.Log("DASH");
+                dashTime = startDashTime;
+                //rb2d.velocity = Vector2.zero;
+            }
+            else{
+                dashTime -= Time.deltaTime;
+                if(isFacingLeft){
+                    rb2d.velocity = Vector2.left * dashSpeed;
+                }
+                else{
+                    rb2d.velocity = Vector2.right * dashSpeed;
+                }
+            }
+        } */
+
+        if(Input.GetKey("k") && !isDashing){
+            StartCoroutine(Dash());
+        }
+
     }
 
     private void Update(){
         Debug.Log("doubleJump value: " + doubleJump);
 
-        if((Input.GetKeyDown("space") && isGrounded) || !doubleJump && Input.GetKeyDown("space")){        //Jump and double jump
+        if((Input.GetKeyDown("space") && isGrounded) || !doubleJump && Input.GetKeyDown("space")){        //Jump
             rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
             doubleJump = !doubleJump;
             isJumping = true;
             jumpTimeCounter = jumpTime;
             animator.Play("player_jump");
         }
-        if(!doubleJump){            //If 
+        if(!doubleJump){            //If double jump has already been used, player is considered to be jumping
             isJumping = true;
         }
         if(isGrounded){             //If grounded, reset double jump
             doubleJump = false;
         }
-        if(Input.GetKey("space") && isJumping){
+        if(Input.GetKey("space") && isJumping){                             //Double jump
             if(jumpTimeCounter > 0){
                 rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
                 jumpTimeCounter -= Time.deltaTime;
@@ -166,13 +196,27 @@ public class PlayerController2D : MonoBehaviour
             Invoke("ResetShoot", shotDelay);
         }
 
-
     }
 
     void ResetShoot(){
         isShooting = false;
         isRunning = false;
         animator.Play("player_idle");
+    }
+
+    IEnumerator Dash(){
+        isDashing = true;
+        rb2d.velocity = new Vector2(rb2d.velocity.x, 0f);
+        if(isFacingLeft){
+            //rb2d.AddForce(new Vector2(dashDistance * -1f, 0f), ForceMode2D.Impulse);
+            rb2d.velocity = new Vector2(-dashSpeed, 0f);
+        }
+        else{
+            //rb2d.AddForce(new Vector2(dashDistance, 0f), ForceMode2D.Impulse);
+            rb2d.velocity = new Vector2(dashSpeed, 0f);
+        }
+        yield return new WaitForSeconds(dashTime);
+        isDashing = false;
     }
 
 }
